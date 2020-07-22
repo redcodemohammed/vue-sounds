561a3980cd53eb538134881786ca6443bff89555
Mohammed <redcodemohammed@gmail.com>
2020-07-23 00:21:48 +0300

    fixed regex to match local files

--- utils/add.js
+++ utils/add.js
@@ -3,24 +3,24 @@
  * @param {String} url - A url that leads to the audio file.
  */
 export default function (audioName, url) {
-    //check if the name is valid:
-    if (typeof audioName !== "string" || audioName.length <= 0) {
-        throw new Error("Invalid audioName");
-    }
+	//check if the name is valid:
+	if (typeof audioName !== "string" || audioName.length <= 0) {
+		throw new Error("Invalid audioName");
+	}
 
-    //check if the name is not used before:
-    if (this.store.getters.getSingleSound(audioName)) {
-        throw new Error("The name of each audio must be unique");
-    }
+	//check if the name is not used before:
+	if (this.store.getters.getSingleSound(audioName)) {
+		throw new Error("The name of each audio must be unique");
+	}
 
-    let urlRegEx = /https?:\/\/(www\.)?[-a-zA-Z0-9\@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()\@:%_\+.~#?&//=]*)/gi;
-    if (typeof url !== "string" || url.length <= 0 || !urlRegEx.test(url)) {
-        throw new Error("Invalid url");
-    }
+	let urlRegEx = /(https?:\/\/(www.)?[-a-zA-Z0-9@:%.+~#=]{1,256}.[a-zA-Z0-9()]{1,6}\b)?([-a-zA-Z0-9()@:%+.~#?&//=]*)/gi;
+	if (typeof url !== "string" || url.length <= 0 || !urlRegEx.test(url)) {
+		throw new Error("Invalid url");
+	}
 
-    //add it to the store:
-    this.store.dispatch("addSounds", {
-        name: audioName,
-        url
-    });
+	//add it to the store:
+	this.store.dispatch("addSounds", {
+		name: audioName,
+		url,
+	});
 }
